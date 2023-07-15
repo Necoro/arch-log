56886ea2533a9060144a758e3c47a4d910bfbebb
René 'Necoro' Neumann <necoro@necoro.eu>
2023-07-15 16:48:17 +0200

    Improved basic structure

--- main.go
+++ main.go
@@ -16,15 +16,45 @@ import (
 var (
 	printVersion bool = false
 	debug        bool = false
+	archForce    bool = false
+	aurForce     bool = false
 )
 
 func init() {
 	flag.BoolVar(&printVersion, "version", printVersion, "print version and exit")
 	flag.BoolVar(&debug, "d", debug, "enable debug output")
+	flag.BoolVar(&archForce, "arch", archForce, "force usage of Arch git")
+	flag.BoolVar(&aurForce, "aur", aurForce, "force usage of AUR")
 }
 
 func formatEntryList(list []entries.Entry) {
 
+}
+
+func handleEntries(what string, pkg string, f func(string) ([]entries.Entry, error)) (bool, error) {
+	log.Debug("Checking ", what)
+
+	if entryList, err := f(pkg); err == nil {
+		formatEntryList(entryList)
+		return false, nil
+	} else if errors.Is(err, entries.ErrNotFound) {
+		log.Debug("Not found on ", what)
+		return true, nil
+	} else {
+		return false, fmt.Errorf("error fetching from %s: %w", what, err)
+	}
+}
+
+func fetch(pkg string) (notfound bool, err error) {
+	if !aurForce {
+		notfound, err = handleEntries("Arch", pkg, arch.GetEntries)
+	}
+
+	if aurForce || (err == nil && notfound && !archForce) {
+		notfound, err = handleEntries("AUR", pkg, aur.GetEntries)
+	}
+
+	return
 }
 
 func run() error {
@@ -38,23 +68,31 @@ func run() error {
 		log.SetDebug()
 	}
 
+	if aurForce && archForce {
+		log.Print("Forced both Arch and AUR, checking both.")
+		aurForce = false
+		archForce = false
+	}
+
 	pkg := flag.Arg(0)
 	if pkg == "" {
 		return fmt.Errorf("no package specified")
 	}
 
-	if entryList, err := arch.GetEntries(pkg); err == nil {
-		formatEntryList(entryList)
-	} else if errors.Is(err, entries.ErrNotFound) {
-		if entryList, err := aur.GetEntries(pkg); err == nil {
-			formatEntryList(entryList)
-		} else if errors.Is(err, entries.ErrNotFound) {
-			return fmt.Errorf("package '%s' could neither be found on Arch nor AUR", pkg)
-		} else {
-			return fmt.Errorf("error fetching from AUR: %w", err)
+	if nf, err := fetch(pkg); err != nil {
+		return err
+	} else if nf {
+		var msg string
+		switch {
+		case aurForce:
+			msg = "could not be found on AUR"
+		case archForce:
+			msg = "could not be found on Arch"
+		default:
+			msg = "could neither be found on Arch nor AUR"
 		}
-	} else {
-		return fmt.Errorf("error fetching from Arch: %w", err)
+
+		return fmt.Errorf("package '%s' %s", pkg, msg)
 	}
 
 	return nil
